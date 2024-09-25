5f8f8f54e5700ad1c218de820f1d18f0581e76cd
MMP Consulting <59109357+mmpconsulting@users.noreply.github.com>
2024-09-25 08:52:29 +0700

    fix

--- report_penerimaan/models/models.py
+++ report_penerimaan/models/models.py
@@ -1,18 +1,14 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class addons\report_penerimaan(models.Model):
-#     _name = 'addons\report_penerimaan.addons\report_penerimaan'
-#     _description = 'addons\report_penerimaan.addons\report_penerimaan'
+class report_penerimaan(models.Model):
+    _inherit = 'stock.picking'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    no_sj = fields.Char(string='No Surat Jalan', required=True)
+
+class report_penerimaan(models.Model):
+    _inherit = 'stock.move'
+
+    notes = fields.Char(string='Keterangan', required=True)
