2c871a5079616a53024a4928e959a4f4e9146561
mmpconsulting <office@mmpconsulting.net>
2024-10-03 15:05:07 +0700

    lwh

--- report_penerimaan/models/models.py
+++ report_penerimaan/models/models.py
@@ -6,9 +6,9 @@ from odoo import models, fields, api
 class report_penerimaan(models.Model):
     _inherit = 'stock.picking'
 
-    no_sj = fields.Char(string='No Surat Jalan', required=True)
+    no_sj = fields.Char(string='No Surat Jalan', required=False)
 
 class report_penerimaan(models.Model):
     _inherit = 'stock.move'
 
-    notes = fields.Char(string='Keterangan', required=True)
+    notes = fields.Char(string='Keterangan', required=False)
